45b09010a786aebf69dfda249c8d1018ff72d4ef
Valeria Kliui <valeria14003@gmail.com>
2024-02-12 21:15:48 +0300

    fix: make mobile design closer to figma

--- src/components/MainPageContent/index.tsx
+++ src/components/MainPageContent/index.tsx
@@ -9,7 +9,7 @@ const { Title, Text } = Typography;
 export const MainPageContent: FC = () =>
     <div className={styles.MainContent} >
         <Space direction={'vertical'} size='large' className={styles.MainSpace}>
-            <Card >
+            <Card className={styles.Cards}>
                 <h6 className='color_blue10'>{`С CleverFit ты сможешь:
                         — планировать свои тренировки на календаре, выбирая тип и уровень нагрузки;
                         — отслеживать свои достижения в разделе статистики, сравнивая свои результаты с нормами и рекордами;
@@ -23,7 +23,7 @@ export const MainPageContent: FC = () =>
                         CleverFit — это не просто приложение, а твой личный помощник в мире фитнеса. Не откладывай на завтра — начни тренироваться уже сегодня!                    </Title>
                 </Card>
 
-                <Row gutter={[16, 8]} className={styles.Cards}>
+                <Row gutter={[16, 8]} className={styles.CardsGrid}>
                     {CARDS_INFO.map(({ title, text, icon, key }) =>
                         <Col key={key} sm={8} xs={24}>
                             <Card bordered={false} className={styles.Card} bodyStyle={{ padding: '11px 30px' }} key={key}
